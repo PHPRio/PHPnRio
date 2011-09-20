952056961167411a2bd11df4eb6f0ee58f72be6a
Igoru <igorsantos07@gmail.com>
2011-09-20 01:14:28 -0300

    making the app work

--- www/index.php
+++ www/index.php
@@ -1,13 +1,14 @@
 <?php
 
-// change the following paths if necessary
-$yii=dirname(__FILE__).'/../../../home/usuario/Download/dev/yii/yii-1.1.8/framework/yii.php';
-$config=dirname(__FILE__).'/protected/config/main.php';
+define('PRODUCTION', !($_SERVER['HTTP_HOST'] == 'localhost' || strpos($_SERVER['HTTP_HOST'], '.dev') >= 0));
 
-// remove the following lines when in production mode
-defined('YII_DEBUG') or define('YII_DEBUG',true);
-// specify how many levels of call stack should be shown in each log message
-defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+$config = dirname(__FILE__).'/../protected/config/main.php';
+$yii = (PRODUCTION)?
+	'/etc/yii/yii-1.1.8/framework/yii.php' :
+	'/var/www/shared/yii/1.1.8-orig/framework/yii.php';
+
+defined('YII_DEBUG') or define('YII_DEBUG', !PRODUCTION);
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', PRODUCTION? 0 : 3);
 
 require_once($yii);
 Yii::createWebApplication($config)->run();
