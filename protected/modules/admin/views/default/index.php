a617d5eb3c782ec40e5d0019a3937bca7200a06d
Igor Santos <igor.santos@hi-midia.com>
2011-09-20 13:10:57 -0300

    changing default page text

--- protected/modules/admin/views/default/index.php
+++ protected/modules/admin/views/default/index.php
@@ -3,13 +3,6 @@ $this->breadcrumbs = array(
 	$this->module->id,
 );
 ?>
-<h1><?php echo $this->uniqueId . '/' . $this->action->id; ?></h1>
+<h1>Home</h1>
 
-<p>
-	This is the view content for action "<?php echo $this->action->id; ?>".
-	The action belongs to the controller "<?php echo get_class($this); ?>"
-	in the "<?php echo $this->module->id; ?>" module.
-</p>
-<p>
-	You may customize this page by editing <tt><?php echo __FILE__; ?></tt>
-</p>
+<p>Selecione uma das opções no menu pra começar a trabalhar! :D</p>
