6684bfb826187d723672a74118a52b49df2b88ca
Igor Santos <igor.santos@hi-midia.com>
2011-09-20 13:52:36 -0300

    fixing user access rules to all pages. closes #2

--- protected/modules/admin/controllers/DefaultController.php
+++ protected/modules/admin/controllers/DefaultController.php
@@ -2,6 +2,32 @@
 
 class DefaultController extends Controller {
 
+	/**
+	 * @return array action filters
+	 */
+	public function filters() {
+		return array(
+			'accessControl', // perform access control for CRUD operations
+		);
+	}
+
+	/**
+	 * Specifies the access control rules.
+	 * This method is used by the 'accessControl' filter.
+	 * @return array access control rules
+	 */
+	public function accessRules() {
+		return array(
+			array('allow',
+				'actions' => array('index'),
+				'users' => array('@'),
+			),
+			array('deny', // deny all users
+				'users' => array('*'),
+			),
+		);
+	}
+
 	public function actionIndex() {
 		$this->render('index');
 	}
