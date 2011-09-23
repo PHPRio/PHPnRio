7eb04b7a09671adad72dff5a57fb4df1bbd1b09a
Igor Santos <igor.santos@hi-midia.com>
2011-09-23 15:02:08 -0300

    creating different paths for different envs

--- www/index.php
+++ www/index.php
@@ -3,13 +3,21 @@ $env = getenv('ENVIRONMENT');
 define('ENV', $env? $env : 'prod');
 define('PRODUCTION', ENV == 'prod');
 
-if (PRODUCTION) {
-	$config = dirname(__FILE__).'/protected/config/main.php';
-	$yii = '/etc/yii/yii-1.1.8/framework/yiilite.php';
-}
-else {
-	$config = dirname(__FILE__).'/../protected/config/main.php';
-	$yii = '/var/www/shared/yii/1.1.8-orig/framework/yii.php';
+switch ($env) {
+	case 'prod':
+		$config = dirname(__FILE__).'/protected/config/main.php';
+		$yii = dirname(__FILE__).'/../etc/yii/yii-1.1.8/framework/yiilite.php';
+	break;
+
+	case 'test':
+		$config = dirname(__FILE__).'/protected/config/main.php';
+		$yii = '/etc/yii/yii-1.1.8/framework/yiilite.php';
+	break;
+
+	case 'dev':
+		$config = dirname(__FILE__).'/../protected/config/main.php';
+		$yii = '/var/www/shared/yii/1.1.8-orig/framework/yii.php';
+	break;
 }
 
 defined('YII_DEBUG') or define('YII_DEBUG', !PRODUCTION);
