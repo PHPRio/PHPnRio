cbd70dbfae032df0d1db7939f6005b66c4e22d74
Igor Santos <igor.santos@hi-midia.com>
2011-09-23 20:21:06 -0300

    missing translation

--- protected/modules/admin/views/layouts/column2.php
+++ protected/modules/admin/views/layouts/column2.php
@@ -9,7 +9,7 @@
 		<div id="sidebar">
 		<?php
 			$this->beginWidget('zii.widgets.CPortlet', array(
-				'title'=>'Operations',
+				'title'=>'Operações',
 			));
 			$this->widget('zii.widgets.CMenu', array(
 				'items'=>$this->menu,
