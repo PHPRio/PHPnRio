845f08107063b25d52460b6b97d2ad0106f83325
Igor Santos <igor.santos@hi-midia.com>
2011-09-23 20:32:28 -0300

    fixing yiic paths

--- protected/yiic.php
+++ protected/yiic.php
@@ -1,7 +1,23 @@
 <?php
+$env = getenv('ENVIRONMENT');
+define('ENV', $env? $env : 'prod');
+define('PRODUCTION', ENV == 'prod');
 
-// change the following paths if necessary
-$yiic=dirname(__FILE__).'/../../../../home/usuario/Download/dev/yii/yii-1.1.8/framework/yiic.php';
-$config=dirname(__FILE__).'/config/console.php';
+switch ($env) {
+	case 'prod':
+		$config = dirname(__FILE__).'/protected/config/main.php';
+		$yiic = dirname(__FILE__).'/../etc/yii/yii-1.1.8/framework/yiic.php';
+	break;
+
+	case 'test':
+		$config = dirname(__FILE__).'/protected/config/main.php';
+		$yiic = '/etc/yii/yii-1.1.8/framework/yiic.php';
+	break;
+
+	case 'dev':
+		$config = dirname(__FILE__).'/../protected/config/main.php';
+		$yiic = '/var/www/shared/yii/1.1.8-orig/framework/yiic.php';
+	break;
+}
 
 require_once($yiic);
