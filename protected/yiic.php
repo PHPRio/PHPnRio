7bdb06a3a5a96370ccc9be7444366df2008c6688
Igor Santos <igor.santos@hi-midia.com>
2011-09-23 20:36:04 -0300

    fixing dumb error: yiic uses console config, not main config

--- protected/yiic.php
+++ protected/yiic.php
@@ -5,17 +5,17 @@ define('PRODUCTION', ENV == 'prod');
 
 switch ($env) {
 	case 'prod':
-		$config = dirname(__FILE__).'/protected/config/main.php';
+		$config = dirname(__FILE__).'/protected/config/console.php';
 		$yiic = dirname(__FILE__).'/../etc/yii/yii-1.1.8/framework/yiic.php';
 	break;
 
 	case 'test':
-		$config = dirname(__FILE__).'/../protected/config/main.php';
+		$config = dirname(__FILE__).'/../protected/config/console.php';
 		$yiic = '/etc/yii/yii-1.1.8/framework/yiic.php';
 	break;
 
 	case 'dev':
-		$config = dirname(__FILE__).'/../protected/config/main.php';
+		$config = dirname(__FILE__).'/../protected/config/console.php';
 		$yiic = '/var/www/shared/yii/1.1.8-orig/framework/yiic.php';
 	break;
 }
