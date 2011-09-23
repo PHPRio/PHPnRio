018fb65828c39a4ca81d3f379411a057d05e6a1e
Igor Santos <igor.santos@hi-midia.com>
2011-09-23 20:34:20 -0300

    fixing test path

--- protected/yiic.php
+++ protected/yiic.php
@@ -10,7 +10,7 @@ switch ($env) {
 	break;
 
 	case 'test':
-		$config = dirname(__FILE__).'/protected/config/main.php';
+		$config = dirname(__FILE__).'/../protected/config/main.php';
 		$yiic = '/etc/yii/yii-1.1.8/framework/yiic.php';
 	break;
 
