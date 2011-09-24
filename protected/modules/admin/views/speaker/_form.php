a971a6a111df582059ea6daab1db7575550ec426
igoru <igorsantos07@gmail.com>
2011-09-24 02:15:56 -0300

    blabla

--- protected/modules/admin/views/speaker/_form.php
+++ protected/modules/admin/views/speaker/_form.php
@@ -17,13 +17,13 @@
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'description'); ?>
-		<?php echo $form->textField($model,'description',array('size'=>60,'maxlength'=>250)); ?>
+		<?php echo $form->textArea($model,'description',array('cols'=>60,'rows'=>3)); ?>
 		<?php echo $form->error($model,'description'); ?>
 	</div>
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'twitter'); ?>
-		<?php echo $form->textField($model,'twitter',array('size'=>30,'maxlength'=>30)); ?>
+		@<?php echo $form->textField($model,'twitter',array('size'=>30,'maxlength'=>30)); ?>
 		<?php echo $form->error($model,'twitter'); ?>
 	</div>
 
