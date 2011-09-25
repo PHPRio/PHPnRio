42121189cd96b7f0a5ceea65df493fc0bf055856
igoru <igorsantos07@gmail.com>
2011-09-25 00:14:56 -0300

    handling default images in actionError

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -39,6 +39,21 @@ class SiteController extends Controller {
 	 */
 	public function actionError() {
 		if ($error = Yii::app()->errorHandler->error) {
+
+			//handling default image for speakers, presentations, etc
+			if ($error['code'] == 404 && strpos($error['message'], '.jpg"') !== false) {
+				preg_match('/".*"/', $error['message'], $path);
+				$path = substr($path[0], 1, -1);
+				$asked_img = Yii::app()->getBasePath(true).'/../'.$path;
+				$is_thumb = strpos($path, '.thumb.') !== false;
+				$default_img = Yii::app()->getBasePath(true).'/../'.dirname($path).'/default'.($is_thumb? '.thumb':'').'.jpg';
+				if (!file_exists($asked_img) && file_exists($default_img)) {
+					header('Content-type: image/jpeg');
+					echo file_get_contents($default_img);
+					exit;
+				}
+			}
+
 			if (Yii::app()->request->isAjaxRequest)
 				echo $error['message'];
 			else
