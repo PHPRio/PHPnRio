41d448a4855aa2b9768153fb3667c93db70c7337
igoru <igorsantos07@gmail.com>
2011-09-30 03:03:32 -0300

    adding static 'inscriptions' page. closes #7

---
+++ protected/views/site/pages/inscricoes.php
@@ -0,0 +1,32 @@
+<!-- meio -->
+<div class="meio">
+
+	<!-- conteúdo -->
+	<div class="conteudo">
+		<div class="titulo">
+			<h2>Inscrições</h2>
+		</div>
+		<h1 style="padding-top:15px;">Já estão abertas as inscrições para o PHP’n Rio 2011.</h1>
+		<p style="padding-top:2px;">&nbsp;</p>
+		<p>Clique no botão abaixo para se inscrever! Você será redirecionado ao site do PagSeguro, onde fará um cadastro (caso já não possua) e escolherá a melhor forma de pagamento. Não perca tempo!</p>
+		<br />
+		<div style="height:80px;">
+			<h2 style="text-align:center;">&nbsp;</h2>
+			<a href="#"><img src="img/bt-inscreva-se.png" alt="inscreva-se" width="165" height="47" border="0" style="float:left; padding-left:277px;" /></a>
+		</div>
+		<br />
+		<p><strong>Observações:</strong></p>
+		<p>
+			O valor das inscrições será revertido integralmente para o conforto do conferecista, não ficando nenhum valor a ser doado para as Instituições Governamentais que estão apoiando o evento.<br />
+			Após confirmada a inscrição, não haverá devolução de valores, mesmo em caso de não comparecimento.
+		</p>
+	</div>
+	<!-- conteúdo -->
+
+	<!-- coluna direita -->
+	<div class="coluna-direita">
+		<?= $this->renderPartial('/layouts/_redes_sociais') ?>
+	</div>
+	<!-- coluna direita -->
+</div>
+<!-- meio -->
