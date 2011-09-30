a1f9470f4b248a46c6f2c9a9b3a673a87b609b12
igoru <igorsantos07@gmail.com>
2011-09-30 03:00:28 -0300

    Adding static 'event' page. refs #18. We still need maps here.

---
+++ protected/views/site/pages/evento.php
@@ -0,0 +1,53 @@
+<!-- meio -->
+<div class="meio">
+
+	<!-- conteúdo -->
+	<div class="conteudo">
+		<div class="titulo">
+			<h2>O Evento</h2>
+		</div>
+		<p style="margin-top:15px;">O PHP é reconhecido como a linguagem de programação mais utilizadas na
+			web, utilizado por mais de 5 milhões de desenvolvedores e responsável por 40% das
+			aplicações web existentes. É a quarta linguagem de programação mais popular em geral,
+			tornando-a uma área de enorme potencial.</p>
+		<br />
+
+		<p>O coração e a alma de um evento é a comunidade que ele serve. PHP’n Rio é
+			mantida por voluntários apaixonados, trazendo um evento anual que beneficia tanto os
+			desenvolvedores quanto suas empresas.</p>
+		<br />
+
+		<p>Por isso, o Grupo Local de Usuários de PHP do Rio de Janeiro (PHP Rio)
+			vem organizando, anualmente, desde 2009, o evento chamado de PHP'n Rio. Trata-
+			se de um evento técnico e com um retorno da comunidade magnífico, de forma que
+			contamos sempre com auditório cheio de profissionais e estudantes, que desempenham
+			uma participação excepcional. O PHP'n Rio está sendo apoiado juridicamente pela
+			Associação Libre de Tecnologias Abertas (ALTA).</p>
+		<br />
+		<h1><img src="imagens/seta.jpg" alt="como chegar" width="8" height="15" style="padding-right:5px;" />Quando?</h1>
+		<p>Sábado dia 05 de novembro de 2011.</p>
+		<br />
+		<h1><img src="imagens/seta.jpg" alt="como chegar" width="8" height="15" style="padding-right:5px;" />Onde?</h1>
+		<p>CEFET/RJ Maracanã</p>
+		<br />
+		<? /* <h1><img src="imagens/seta.jpg" alt="como chegar" width="8" height="15" style="padding-right:5px;" />Como Chegar?</h1>
+		<p>Ônibus:</p>
+		<br />
+		<p>Metrô:</p>
+		<br />
+		<p>Trem:</p>
+		<br />
+		<p>Carro:</p>
+		<br /> */ ?>
+		<h2>Valor: R$ 30,00</h2><a href="#"><img src="img/bt-inscreva-se.png" alt="inscreva-se" width="165" height="47" border="0" style="float:left; padding-left:277px;" /></a>
+		<br />
+	</div>
+	<!-- conteúdo -->
+
+	<!-- coluna direita -->
+	<div class="coluna-direita">
+		<?= $this->renderPartial('/layouts/_redes_sociais') ?>
+	</div>
+	<!-- coluna direita -->
+</div>
+<!-- meio -->
