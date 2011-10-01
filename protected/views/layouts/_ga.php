72af80e1e4b647c84954d0a53379eed791b2a4dc
igoru <igorsantos07@gmail.com>
2011-10-01 15:47:08 -0300

    adding GetClicky too.

--- protected/views/layouts/_ga.php
+++ protected/views/layouts/_ga.php
@@ -11,3 +11,15 @@
   })();
 
 </script>
+
+<script type="text/javascript">
+var clicky_site_ids = clicky_site_ids || [];
+clicky_site_ids.push(66485995);
+(function() {
+  var s = document.createElement('script');
+  s.type = 'text/javascript';
+  s.async = true;
+  s.src = '//static.getclicky.com/js';
+  ( document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0] ).appendChild( s );
+})();
+</script>
