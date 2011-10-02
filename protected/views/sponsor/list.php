45194f2d0f3097bdb70d809cfc3d4eaac97da5d0
igoru <igorsantos07@gmail.com>
2011-10-02 19:43:48 -0300

    showing 'sponsors' header if there is or there's not sponsors

--- protected/views/sponsor/list.php
+++ protected/views/sponsor/list.php
@@ -3,20 +3,20 @@
 
 	<!-- conteúdo -->
 	<div class="conteudo">
-		<? if (isset($this->sponsors['sponsor']) && is_array($this->sponsors['sponsor'])): ?>
 		<div class="titulo">
-			<h2>Patrocinadores e Apoiadores</h2>
+			<h2>Patrocinadores</h2>
 			<p>O PHP'n Rio 2011 agradece todos os patrocinadores e apoiadores do evento.</p>
 			<p>Caso queira a sua marca associada ao evento, entre em contato através do e-mail <a href="mailto:phpnrio@phprio.org">phpnrio@phprio.org</a>.</p>
 		</div>
-		<? foreach ($this->sponsors['sponsor'] as $sponsor) echo $this->renderPartial('/sponsor/_box_sponsor', compact('sponsor')) ?>
+		<? if (isset($this->sponsors['sponsor']) && is_array($this->sponsors['sponsor'])): ?>
+			<? foreach ($this->sponsors['sponsor'] as $sponsor) echo $this->renderPartial('/sponsor/_box_sponsor', compact('sponsor')) ?>
 		<? endif ?>
 
 		<? if (isset($this->sponsors['supporter']) && is_array($this->sponsors['supporter'])): ?>
-		<div class="titulo">
-			<h2>Apoiadores</h2>
-		</div>
-		<? foreach ($this->sponsors['supporter'] as $sponsor) echo $this->renderPartial('/sponsor/_box_sponsor', compact('sponsor')) ?>
+			<div class="titulo">
+				<h2>Apoiadores</h2>
+			</div>
+			<? foreach ($this->sponsors['supporter'] as $sponsor) echo $this->renderPartial('/sponsor/_box_sponsor', compact('sponsor')) ?>
 		<? endif ?>
 	</div>
 	<!-- conteúdo -->
