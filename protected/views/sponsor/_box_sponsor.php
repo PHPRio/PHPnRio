e8f5555fda05473c7909f9969c5165e62514b8fe
igoru <igorsantos07@gmail.com>
2011-10-07 02:03:58 -0300

    adding link to sponsor image and removing useless copy-pasted code

--- protected/views/sponsor/_box_sponsor.php
+++ protected/views/sponsor/_box_sponsor.php
@@ -1,18 +1,14 @@
 <a name="<?=$sponsor->id?>"></a><a name="<?=$sponsor->slug?>"></a>
 <div class="pagina-palestrantes">
 	<h1>
-		<img src="<?=$sponsor->getImageUrl('imageFile')?>" alt="<?=$sponsor->name?>" width="115" height="79" align="left" style="margin-right:15px;" />
+		<a href="<?=$sponsor->url?>" target="_blank">
+			<img src="<?=$sponsor->getImageUrl('imageFile')?>" alt="<?=$sponsor->name?>" width="115" height="79" align="left" style="margin-right:15px;" />
+		</a>
 		<img src="img/seta.jpg" alt="palestras" width="8" height="15" style="margin-right:5px;" />
 		<?=$sponsor->name?>
 	</h1>
 
-		<h3><?=CHtml::link($sponsor->url, $sponsor->url)?></h3>
-
-	<? if (!empty($sponsor->twitter)) { ?>
-		<p style="color:#60a7aa">
-			<a href="http://<?=$sponsor->twitterLink?>" target="_blank" style="color:#60a7aa;">@<?=$sponsor->twitter?></a>
-		</p>
-	<? } ?>
+	<h3><?=CHtml::link($sponsor->url, $sponsor->url, array('target' => '_blank'))?></h3>
 
 	<p><?=$sponsor->description?></p>
 
