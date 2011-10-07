293603181f67eda5fa19fe6f9d55456d0973fef5
igoru <igorsantos07@gmail.com>
2011-10-07 02:30:53 -0300

    adding title and target="_blank" to sponsors' boxes

--- protected/views/layouts/_box_sponsor.php
+++ protected/views/layouts/_box_sponsor.php
@@ -1,5 +1,5 @@
 <div class="box-logos-<?=$sponsor? 'patrocinio' : 'apoio'?>">
-	<a href="<?=$link?>">
+	<a href="<?=$link?>" title="<?=$name?>"<?=(strpos($link, 'http') === 0)? ' target="_blank"' : ''?>>
 		<img src="<?=$img?>" alt="<?=$name?>" width="115" height="79" border="0" />
 	</a>
 </div>
