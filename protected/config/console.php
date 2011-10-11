ec953856a173cc37b847695e1170cc41a5d04878
Igor Santos <igor.santos@hi-midia.com>
2011-10-11 14:43:55 -0300

    fixing use of USE_REMOTE_DB env var

--- protected/config/console.php
+++ protected/config/console.php
@@ -4,6 +4,6 @@ return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>"PHP'n Rio 2011 Command Line Tool",
 	'components'=>array(
-		'db' => require (isset($_ENV['USE_REMOTE_DB']) && $_ENV['USE_REMOTE_DB'])? '_database_remote.php' : '_database.php',
+		'db' => require (getenv('USE_REMOTE_DB'))? '_database_remote.php' : '_database.php',
 	),
 );
