a131a71820325fdfbd0d45a4861813728cb3cd62
Igor Santos <igor.santos@hi-midia.com>
2011-10-11 19:13:27 -0300

    changing 'presentations' link to 'schedule' in the main menu

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -33,10 +33,9 @@
 					<div id="menu">
 						<ul>
 							<?php /* Submenu Grade, Palestrantes, Local, Informações */ ?>
-							<li><?=CHtml::link('O EVENTO',			array('site/page', 'view' => 'evento'))?></li>
+							<li><?=CHtml::link('O EVENTO',		array('site/page', 'view' => 'evento'))?></li>
 							<li><?=CHtml::link('INSCRIÇÕES',		array('site/page', 'view' => 'inscricoes'))?></li>
-							<? /*<li><?=CHtml::link('PROGRAMAÇÃO',		array('site/page', 'view' => 'programacao'))?></li> */?>
-							<li><?=CHtml::link('PALESTRAS',	array('presentation/list'))?></li>
+							<li><?=CHtml::link('GRADE',			array('site/page', 'view' => 'programacao'))?></li>
 							<li><?=CHtml::link('PATROCINADORES',	array('sponsor/list'))?></li>
 							<li><?=CHtml::link('ORGANIZAÇÃO',		array('teamMember/list'))?></li>
 						</ul>
