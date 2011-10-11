d68281d49b74d43d6faa2fae784ad9edd54d8bf7
igoru <igorsantos07@gmail.com>
2011-10-11 00:15:31 -0300

    adding right column to the schedule page

--- protected/views/site/pages/programacao.php
+++ protected/views/site/pages/programacao.php
@@ -1,82 +1,94 @@
 <!-- meio -->
 <div class="meio">
-<!-- conteúdo -->
-<div class="conteudo">
-<div class="titulo">
-  <h2>Programação</h2>
+	<!-- conteúdo -->
+	<div class="conteudo">
+		<div class="titulo">
+			<h2>Programação</h2>
+		</div>
+
+		<!-- box programação -->
+		<div class="programacao">
+			<table width="706" border="0" align="center" cellpadding="1" cellspacing="1" style="border:#999999 solid 1px; padding:5px; text-align:center;">
+				<tr style="background-color:#0070C1; border:#999999 solid 1px; color:#FFFFFF;">
+					<td width="94" height="61">Horário</td>
+					<td width="150">Auditório</td>
+					<td width="150">Sala Vip</td>
+					<td width="150">Treinamento Jedi 1</td>
+					<td width="150">Treinamento Jedi 2</td>
+				</tr>
+				<tr style="margin:5px;">
+					<td height="40" bgcolor="#EBEBEB">09:00/09:20</td>
+					<td colspan="4" bgcolor="#FFFF99">Credenciamento e Breakfast – Área aberta</td>
+				</tr>
+				<tr>
+				<tr style="margin:5px;">
+					<td height="40" bgcolor="#EBEBEB">09:20/09:30</td>
+					<td colspan="4" bgcolor="#FFFF99">ABERTURA – Auditório Principal</td>
+				</tr>
+				<tr>
+					<td height="40" bgcolor="#EBEBEB">09:30/10:30</td>
+					<td bgcolor="#EBEBEB">Palestra</td>
+					<td bgcolor="#EBEBEB">Palestra</td>
+					<td rowspan="3" bgcolor="#D9F2FF">Mini Curso</td>
+					<td rowspan="3" bgcolor="#D9F2FF">Mini Curso</td>
+				</tr>
+				<tr>
+					<td height="40" bgcolor="#EBEBEB">10:30/11:30</td>
+					<td bgcolor="#EBEBEB">Palestra</td>
+					<td bgcolor="#EBEBEB">Palestra</td>
+				</tr>
+				<tr>
+					<td height="40" bgcolor="#EBEBEB">11:30/12:30</td>
+					<td bgcolor="#EBEBEB">Palestra</td>
+					<td bgcolor="#EBEBEB">Palestra</td>
+				</tr>
+				<tr>
+					<td height="40" bgcolor="#EBEBEB">12:30/13:30</td>
+					<td colspan="4" bgcolor="#FFFF99">ALMOÇO</td>
+				</tr>
+				<tr>
+					<td height="40" bgcolor="#EBEBEB">13:30/14:30</td>
+					<td bgcolor="#EBEBEB">Palestra</td>
+					<td bgcolor="#EBEBEB">Palestra</td>
+					<td rowspan="3" bgcolor="#D9F2FF">Mini Curso</td>
+					<td rowspan="3" bgcolor="#D9F2FF">Mini Curso</td>
+				</tr>
+				<tr>
+					<td height="40" bgcolor="#EBEBEB">14:30/15:30</td>
+					<td bgcolor="#EBEBEB">Palestra</td>
+					<td bgcolor="#EBEBEB">Palestra</td>
+				</tr>
+				<tr>
+					<td height="40" bgcolor="#EBEBEB">15:30/16:30</td>
+					<td bgcolor="#EBEBEB">Palestra</td>
+					<td bgcolor="#EBEBEB">Palestra</td>
+				</tr>
+				<tr>
+					<td height="40" bgcolor="#EBEBEB">16:30/16:50</td>
+					<td colspan="4" bgcolor="#FFFF99">Coffe-break</td>
+				</tr>
+				<tr>
+					<td height="40" bgcolor="#EBEBEB">17:00/18:20</td>
+					<td colspan="4" bgcolor="#EBEBEB">Palestra Final - Auditório Principal</td>
+				</tr>
+				<tr>
+					<td height="40" bgcolor="#EBEBEB">18:20/18:30</td>
+					<td colspan="4" bgcolor="#FFFF99">Encerramento - Auditório Principal</td>
+				</tr>
+			</table>
+
+		</div>
+		<!-- box programação -->
+
+	</div>
+	<!-- conteúdo -->
+
+	<!-- coluna direita -->
+	<div class="coluna-direita">
+		<?= $this->renderPartial('/layouts/_box_inscricoes') ?>
+		<?= $this->renderPartial('/layouts/_redes_sociais') ?>
+	</div>
+	<!-- coluna direita -->
+
 </div>
-
-<!-- box programação -->
-<div class="programacao">
-<table width="706" border="0" align="center" cellpadding="1" cellspacing="1" style="border:#999999 solid 1px; padding:5px; text-align:center;">
-  <tr style="background-color:#0070C1; border:#999999 solid 1px; color:#FFFFFF;">
-    <td width="94" height="61">Horário</td>
-    <td width="150">Auditório</td>
-    <td width="150">Sala Vip</td>
-    <td width="150">Treinamento Jedi 1</td>
-    <td width="150">Treinamento Jedi 2</td>
-  </tr>
-  <tr style="margin:5px;">
-    <td height="40" bgcolor="#EBEBEB">09:00/09:20</td>
-    <td colspan="4" bgcolor="#FFFF99">Credenciamento e Breakfast – Área aberta</td>
-    </tr>
-  <tr>  
-  <tr style="margin:5px;">
-    <td height="40" bgcolor="#EBEBEB">09:20/09:30</td>
-    <td colspan="4" bgcolor="#FFFF99">ABERTURA – Auditório Principal</td>
-    </tr>
-  <tr>
-    <td height="40" bgcolor="#EBEBEB">09:30/10:30</td>
-    <td bgcolor="#EBEBEB">Palestra</td>
-    <td bgcolor="#EBEBEB">Palestra</td>
-    <td rowspan="3" bgcolor="#D9F2FF">Mini Curso</td>
-    <td rowspan="3" bgcolor="#D9F2FF">Mini Curso</td>
-  </tr>
-  <tr>
-    <td height="40" bgcolor="#EBEBEB">10:30/11:30</td>
-    <td bgcolor="#EBEBEB">Palestra</td>
-    <td bgcolor="#EBEBEB">Palestra</td>
-    </tr>
-  <tr>
-    <td height="40" bgcolor="#EBEBEB">11:30/12:30</td>
-    <td bgcolor="#EBEBEB">Palestra</td>
-    <td bgcolor="#EBEBEB">Palestra</td>
-    </tr>
-  <tr>
-    <td height="40" bgcolor="#EBEBEB">12:30/13:30</td>
-    <td colspan="4" bgcolor="#FFFF99">ALMOÇO</td>
-    </tr>
-  <tr>
-    <td height="40" bgcolor="#EBEBEB">13:30/14:30</td>
-    <td bgcolor="#EBEBEB">Palestra</td>
-    <td bgcolor="#EBEBEB">Palestra</td>
-    <td rowspan="3" bgcolor="#D9F2FF">Mini Curso</td>
-    <td rowspan="3" bgcolor="#D9F2FF">Mini Curso</td>
-  </tr>
-  <tr>
-    <td height="40" bgcolor="#EBEBEB">14:30/15:30</td>
-    <td bgcolor="#EBEBEB">Palestra</td>
-    <td bgcolor="#EBEBEB">Palestra</td>
-    </tr>
-  <tr>
-    <td height="40" bgcolor="#EBEBEB">15:30/16:30</td>
-    <td bgcolor="#EBEBEB">Palestra</td>
-    <td bgcolor="#EBEBEB">Palestra</td>
-    </tr>    
-  <tr>
-    <td height="40" bgcolor="#EBEBEB">16:30/16:50</td>
-    <td colspan="4" bgcolor="#FFFF99">Coffe-break</td>
-    </tr>    
-  <tr>
-    <td height="40" bgcolor="#EBEBEB">17:00/18:20</td>
-    <td colspan="4" bgcolor="#EBEBEB">Palestra Final - Auditório Principal</td>
-</tr>
-  <tr>
-    <td height="40" bgcolor="#EBEBEB">18:20/18:30</td>
-    <td colspan="4" bgcolor="#FFFF99">Encerramento - Auditório Principal</td>
-    </tr>
-</table>
-
-</div>
-<!-- box programação -->
-
+<!-- meio -->
