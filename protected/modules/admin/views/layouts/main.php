3d21a6dfdac09ca6249ec5e00927ab7b0ac122ae
igoru <igorsantos07@gmail.com>
2011-10-11 02:43:37 -0300

    formatting file and adding new jQuery from google CDN

--- protected/modules/admin/views/layouts/main.php
+++ protected/modules/admin/views/layouts/main.php
@@ -1,60 +1,67 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="language" content="en" />
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta name="language" content="en" />
 
-	<!-- blueprint CSS framework -->
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print" />
-	<!--[if lt IE 8]>
+		<!-- blueprint CSS framework -->
+		<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" />
+		<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print" />
+		<!--[if lt IE 8]>
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection" />
 	<![endif]-->
 
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
+		<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
+		<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
 
-	<title>PHP'n Rio 2011 Admin - <?=substr($this->getRoute(), strlen('admin/'))?></title>
-</head>
+		<?php
+			echo CGoogleApi::init();
+			echo CHtml::script(CGoogleApi::load('jquery', '1.6.4'));
+		?>
 
-<body>
+		<title>PHP'n Rio 2011 Admin - <?= substr($this->getRoute(), strlen('admin/')) ?></title>
+	</head>
 
-<div class="container" id="page">
+	<body>
 
-	<div id="header">
-		<div id="logo"><?php echo CHtml::encode(Yii::app()->name); ?></div>
-	</div><!-- header -->
+		<div class="container" id="page">
 
-	<div id="mainmenu">
-		<?php $this->widget('zii.widgets.CMenu',array(
-			'items'=>array(
-				array('label'=>'Notícias', 'url' => $this->createUrl('news/index')),
-				array('label'=>'Palestrantes', 'url' => $this->createUrl('speaker/index')),
-				array('label'=>'Palestras', 'url' => $this->createUrl('presentation/index')),
-				array('label'=>'Patrocinadores', 'url' => $this->createUrl('sponsor/index')),
-				array('label'=>'Membros da Equipe', 'url' => $this->createUrl('teamMember/index')),
-				array('label'=>'Usuários', 'url'=> $this->createUrl('user/index')),
+			<div id="header">
+				<div id="logo"><?php echo CHtml::encode(Yii::app()->name); ?></div>
+			</div><!-- header -->
 
-				array('label'=>'Login', 'url'=>array('login'), 'visible'=>Yii::app()->user->isGuest),
-				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('default/logout'), 'visible'=>!Yii::app()->user->isGuest)
-			),
-		)); ?>
-	</div><!-- mainmenu -->
-	<?php if(isset($this->breadcrumbs)):?>
-		<?php $this->widget('zii.widgets.CBreadcrumbs', array(
-			'links'=>$this->breadcrumbs,
-		)); ?><!-- breadcrumbs -->
-	<?php endif?>
+			<div id="mainmenu">
+				<?php
+				$this->widget('zii.widgets.CMenu', array(
+					'items' => array(
+						array('label' => 'Notícias', 'url' => $this->createUrl('news/index')),
+						array('label' => 'Palestrantes', 'url' => $this->createUrl('speaker/index')),
+						array('label' => 'Palestras', 'url' => $this->createUrl('presentation/index')),
+						array('label' => 'Patrocinadores', 'url' => $this->createUrl('sponsor/index')),
+						array('label' => 'Membros da Equipe', 'url' => $this->createUrl('teamMember/index')),
+						array('label' => 'Usuários', 'url' => $this->createUrl('user/index')),
+						array('label' => 'Login', 'url' => array('login'), 'visible' => Yii::app()->user->isGuest),
+						array('label' => 'Logout (' . Yii::app()->user->name . ')', 'url' => array('default/logout'), 'visible' => !Yii::app()->user->isGuest)
+					),
+				));
+				?>
+			</div><!-- mainmenu -->
+			<?php if (isset($this->breadcrumbs)): ?>
+				<?php
+				$this->widget('zii.widgets.CBreadcrumbs', array(
+					'links' => $this->breadcrumbs,
+				));
+				?><!-- breadcrumbs -->
+			<?php endif ?>
 
-	<?php echo $content; ?>
+				<?php echo $content; ?>
 
-		<div id="footer">
+			<div id="footer">
 			Criado com <a href="http://www.yiiframework.com">Yii Framework</a><br />
-			<a href="https://github.com/igorsantos07/PHP-n-Rio-2011/">Código-fonte do site</a> ||
-			<a href="https://github.com/igorsantos07/PHP-n-Rio-2011/issues/">Tarefas e bugs</a>
-		</div>
+				<a href="https://github.com/igorsantos07/PHP-n-Rio-2011/">Código-fonte do site</a> ||
+				<a href="https://github.com/igorsantos07/PHP-n-Rio-2011/issues/">Tarefas e bugs</a>
+			</div>
 
-</div><!-- page -->
-
-</body>
+		</div><!-- page -->
+	</body>
 </html>
