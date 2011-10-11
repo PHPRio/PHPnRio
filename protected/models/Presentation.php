c3e14b70754c1e8ab569b37955df49855edcf524
Igor Santos <igor.santos@hi-midia.com>
2011-10-11 12:42:30 -0300

    fixing presentation model from unfinished changes

--- protected/models/Presentation.php
+++ protected/models/Presentation.php
@@ -8,6 +8,7 @@
  * @property string $title
  * @property string $description
  * @property integer $period
+ * @property integer $speaker_id
  * @property string $slug
  *
  * @property virt-string $periodTime
@@ -68,6 +69,7 @@ class Presentation extends CActiveRecord {
 		// will receive user inputs.
 		return array(
 			array('title, description, speaker_id', 'required'),
+			array('speaker_id', 'numerical', 'integerOnly'=>true),
 			array('period', 'numerical', 'integerOnly'=>true, 'allowEmpty' => true),
 			array('title', 'length', 'max'=>100),
 			// The following rule is used by search().
@@ -83,7 +85,7 @@ class Presentation extends CActiveRecord {
 		// NOTE: you may need to adjust the relation name and the related
 		// class name for the relations automatically generated below.
 		return array(
-			'speakers' => array(self::MANY_MANY, 'Speaker', 'speaker_presentation(speaker_id, presentation_id)'),
+			'speaker' => array(self::BELONGS_TO, 'Speaker', 'speaker_id'),
 		);
 	}
 
@@ -96,10 +98,10 @@ class Presentation extends CActiveRecord {
 			'title' => 'Título',
 			'description' => 'Descrição',
 			'image' => 'Imagem',
-			'period' => 'Horário',
-			'periodTime' => 'Horário',
-			'speakers' => 'Palestrante(s)',
-			'speakersNames' => 'Palestrante(s)',
+			'begin' => 'Início',
+			'end' => 'Fim',
+			'speaker_id' => 'Palestrante',
+			'speaker.name' => 'Palestrante',
 		);
 	}
 
@@ -116,17 +118,14 @@ class Presentation extends CActiveRecord {
 		$criteria->compare('id',$this->id);
 		$criteria->compare('title',$this->title,true);
 		$criteria->compare('description',$this->description,true);
+		$criteria->compare('speaker_id',$this->speaker_id);
 
-		return new CActiveDataProvider($this, array('criteria' => $criteria));
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
 	}
 
 	public function getImageName() { return $this->id.'.jpg'; }
 
 	public function getPeriodTime() { return self::$periods[isset($this->period)? $this->period : 0]; }
-
-	public function getSpeakersNames() {
-		$names = array();
-		foreach ($this->speakers as $speaker) $names[] = $speaker->name;
-		return implode("\n", $names);
-	}
 }
