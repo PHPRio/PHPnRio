17dc620646727e0826f3752d29c71fd9ec20bf52
raphaeldealmeida <jaguarnet7@gmail.com>
2011-10-13 09:53:33 -0300

    divulgação

--- protected/views/site/pages/divulgacao.php
+++ protected/views/site/pages/divulgacao.php
@@ -1,6 +1,6 @@
 <style>
-.conteudo img {
-  text-ali
+.conteudo p {
+  text-align: center;
 }
 </style>
 <!-- meio -->
@@ -24,21 +24,26 @@
       <img width="238" height="60" src="img/banner_phpnrio_238_60.gif" alt="PHP'n Rio 2011 - PHP'n Rio 2011 - Dia 5 de Novembro no CEFET-RJ campus Maracanã" />
     </p>
     <br />
-    <p>Banner 468x60</p>
-    <img width="468" height="60" src="img/banner_phpnrio_468_60.gif" alt="PHP'n Rio 2011 - PHP'n Rio 2011 - Dia 5 de Novembro no CEFET-RJ campus Maracanã" />
+    <p>Banner 468x60<br />
+      <img width="468" height="60" src="img/banner_phpnrio_468_60.gif" alt="PHP'n Rio 2011 - PHP'n Rio 2011 - Dia 5 de Novembro no CEFET-RJ campus Maracanã" />
+    </p>
     <br />
-    <p>Banner 120x240</p>
-    <img width="120" height="240" src="img/banner_phpnrio_120_240_euapoio.gif" alt="PHP'n Rio 2011 - PHP'n Rio 2011 - Dia 5 de Novembro no CEFET-RJ campus Maracanã" />
-    <img width="120" height="240" src="img/banner_phpnrio_120_240_eupatrocino.gif" alt="PHP'n Rio 2011 - PHP'n Rio 2011 - Dia 5 de Novembro no CEFET-RJ campus Maracanã" />
-    <img width="120" height="240" src="img/banner_phpnrio_120_240_euvou.gif" alt="PHP'n Rio 2011 - PHP'n Rio 2011 - Dia 5 de Novembro no CEFET-RJ campus Maracanã" />
-    <img width="120" height="240" src="img/banner_phpnrio_120_240_soupalestrante.gif" alt="PHP'n Rio 2011 - PHP'n Rio 2011 - Dia 5 de Novembro no CEFET-RJ campus Maracanã" />
+    <p>Banner 120x240<br />
+      <img width="120" height="240" src="img/banner_phpnrio_120_240_euapoio.gif" alt="PHP'n Rio 2011 - PHP'n Rio 2011 - Dia 5 de Novembro no CEFET-RJ campus Maracanã" />
+      <img width="120" height="240" src="img/banner_phpnrio_120_240_eupatrocino.gif" alt="PHP'n Rio 2011 - PHP'n Rio 2011 - Dia 5 de Novembro no CEFET-RJ campus Maracanã" />
+      <img width="120" height="240" src="img/banner_phpnrio_120_240_euvou.gif" alt="PHP'n Rio 2011 - PHP'n Rio 2011 - Dia 5 de Novembro no CEFET-RJ campus Maracanã" />
+      <img width="120" height="240" src="img/banner_phpnrio_120_240_soupalestrante.gif" alt="PHP'n Rio 2011 - PHP'n Rio 2011 - Dia 5 de Novembro no CEFET-RJ campus Maracanã" />
+    </p>
     <br />
-    <p>Banner 190x190</p>
+    <p>Banner 190x190<br />
     <img width="190" height="190" src="img/banner_phpnrio_190_190_sqr.png" alt="PHP'n Rio 2011 - PHP'n Rio 2011 - Dia 5 de Novembro no CEFET-RJ campus Maracanã" />
+    </p>
     <br /><br />
     <h1><img src="imagens/seta.jpg" alt="grade" width="8" height="15" style="padding-right:5px;" />Cartaz A4 para impressão</h1>
-    <img width="265" height="375" src="img/a4_265_375.png" alt="PHP'n Rio 2011 - PHP'n Rio 2011 - Dia 5 de Novembro no CEFET-RJ campus Maracanã" />
-    <p><a href="http://phpnrio.com.br/a4.png">Download em alta resolução(6MB)</a></p>
+    <p>
+      <img width="265" height="375" src="img/a4_265_375.png" alt="PHP'n Rio 2011 - PHP'n Rio 2011 - Dia 5 de Novembro no CEFET-RJ campus Maracanã" /><br />
+      <a href="http://phpnrio.com.br/a4.png">Download em alta resolução(6MB)</a>
+    </p>
 
 	</div>
 	<!-- conteúdo -->
