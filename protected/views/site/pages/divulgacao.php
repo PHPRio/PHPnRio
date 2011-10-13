cb5ce7e06425ff597f18fec1d755fc76975589c9
raphaeldealmeida <jaguarnet7@gmail.com>
2011-10-13 09:50:49 -0300

    divulgação

--- protected/views/site/pages/divulgacao.php
+++ protected/views/site/pages/divulgacao.php
@@ -1,3 +1,8 @@
+<style>
+.conteudo img {
+  text-ali
+}
+</style>
 <!-- meio -->
 <div class="meio">
 
@@ -6,7 +11,6 @@
     <div class="titulo">
       <h2>Divulgação</h2>
     </div>
-    <br /><br />
     <p style="margin-top:15px;">Ajude na divulgação do PHP'n Rio 2011.</p>
     <p>Divulgue o evento no seu local de trabalho, em apresentações, na sua escola/universidade e nas mídias sociais.</p>
     <br />
@@ -16,8 +20,9 @@
     <h1><img src="imagens/seta.jpg" alt="grade" width="8" height="15" style="padding-right:5px;" />Banner para site</h1>
     <p>Coloque no seu site o nosso banner</p>
     <br />    
-    <p>Banner 238x60</p>
-    <img width="238" height="60" src="img/banner_phpnrio_238_60.gif" alt="PHP'n Rio 2011 - PHP'n Rio 2011 - Dia 5 de Novembro no CEFET-RJ campus Maracanã" />
+    <p>Banner 238x60<br />
+      <img width="238" height="60" src="img/banner_phpnrio_238_60.gif" alt="PHP'n Rio 2011 - PHP'n Rio 2011 - Dia 5 de Novembro no CEFET-RJ campus Maracanã" />
+    </p>
     <br />
     <p>Banner 468x60</p>
     <img width="468" height="60" src="img/banner_phpnrio_468_60.gif" alt="PHP'n Rio 2011 - PHP'n Rio 2011 - Dia 5 de Novembro no CEFET-RJ campus Maracanã" />
@@ -31,7 +36,7 @@
     <p>Banner 190x190</p>
     <img width="190" height="190" src="img/banner_phpnrio_190_190_sqr.png" alt="PHP'n Rio 2011 - PHP'n Rio 2011 - Dia 5 de Novembro no CEFET-RJ campus Maracanã" />
     <br /><br />
-    <h1><img src="imagens/seta.jpg" alt="grade" width="8" height="15" style="padding-right:5px;" />Cartaz para impressão</h1>
+    <h1><img src="imagens/seta.jpg" alt="grade" width="8" height="15" style="padding-right:5px;" />Cartaz A4 para impressão</h1>
     <img width="265" height="375" src="img/a4_265_375.png" alt="PHP'n Rio 2011 - PHP'n Rio 2011 - Dia 5 de Novembro no CEFET-RJ campus Maracanã" />
     <p><a href="http://phpnrio.com.br/a4.png">Download em alta resolução(6MB)</a></p>
 
