e2da73d82b880f0c635260c5ae6682fd8fba819b
igoru <igorsantos07@gmail.com>
2011-10-19 02:04:20 -0200

    adding route to gii module

--- protected/config/main.php
+++ protected/config/main.php
@@ -40,6 +40,7 @@ $config = array(
 				'palestras' => 'presentation/list',	'palestras/grade' => 'presentation/grid',
 				'noticias' => 'news/list',			'noticia/<data>' => 'news/view',
 				'/admin' => 'admin/default',
+				'/gii' => 'gii/default',
 				'/<view>' => 'site/page',
 //				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
 //				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
