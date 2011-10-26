5f0beca1dc16c9dbfa5f5460972cde7a57b9f884
Igoru <igorsantos07@gmail.com>
2011-10-26 04:16:55 -0200

    adding basic attendee's form under the schedule table and adding some notes under it too. now the schedule page saves attendee and presentations relationship with the transaction to the database

--- protected/controllers/ScheduleController.php
+++ protected/controllers/ScheduleController.php
@@ -9,16 +9,21 @@ class ScheduleController extends Controller {
 	}
 
 	public function actionIndex() {
-		$presentations = Presentation::model()->findAll();
-		foreach($presentations as $pres) $this->presentations[$pres->slug] = $pres;
-		$this->render('index');
+		if (isset($_SESSION['transaction']) && !$_SESSION['transaction']) {
+			$transaction = Transaction::model()->findByAttributes(array('code' => $_SESSION['transaction']));
+		}
+		else {
+			$transaction = false;
+		}
+
+		$this->render('index', compact('transaction'));
 	}
 
 	public function actionIdentifyTransaction() {
-		$attendee = Transaction::model()->findByAttributes(array('code' => $_POST['transaction']));
-		if (sizeof($attendee)) {
+		$trans = Transaction::model()->findByAttributes(array('code' => $_POST['transaction']));
+		if (sizeof($trans)) {
 			$_SESSION['transaction'] = $_POST['transaction'];
-			$_SESSION['first_name'] = strtok($attendee->name, ' ');
+			$_SESSION['first_name'] = strtok($trans->name, ' ');
 		}
 		else {
 			$_SESSION['transaction'] = false;
@@ -27,9 +32,40 @@ class ScheduleController extends Controller {
 		$this->redirect('/grade');
 	}
 
+	public function actionSetPresentationsAndAttendees() {
+		$transaction = self::findTransaction($_SESSION['transaction']);
+
+		$schedule_array = require dirname(dirname(__FILE__))."/config/schedule_array.php";
+		$attendees = array_pop($_POST);
+		$presentations = $_POST;
+
+		$presentation_ids = array();
+		foreach($presentations as $pos => $number) {
+			$type = ($pos[0] == 'p')? 'presentations' : 'workshops';
+			$presentation_ids[] = $schedule_array[$type][$pos[1]-1][$number-1];
+		}
+		$presentation_ids = array_merge(array_unique($presentation_ids));
+		$transaction->presentations = $presentation_ids;
+		var_dump($transaction->save());
+
+		Attendee::model()->deleteAllByAttributes(array('transaction_id' => $transaction->id));
+		foreach ($attendees as $attendee) {
+			$att = new Attendee;
+			$att->attributes = $attendee;
+			$att->transaction_id = $transaction->id;
+			var_dump($att->save());
+		}
+
+		$this->redirect('/grade');
+	}
+
 	public function afterAction(CAction $action) {
 		if (isset($_SESSION['transaction']) && $action->id == 'index' && $_SESSION['transaction'] === false)
 			unset($_SESSION['transaction']);
+	}
+
+	private static function findTransaction($code) {
+		return Transaction::model()->findByAttributes(array('code' => $code));
 	}
 
 }
