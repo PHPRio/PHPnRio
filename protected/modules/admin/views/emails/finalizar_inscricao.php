e4889c4de7bc73d67450884b3da9415ee3f6667e
Igor Santos <igor.santos@hi-midia.com>
2011-10-27 14:15:29 -0200

    fixing email width. refs #64

--- protected/modules/admin/views/emails/finalizar_inscricao.php
+++ protected/modules/admin/views/emails/finalizar_inscricao.php
@@ -5,23 +5,25 @@
 	</head>
 	<body>
 		<img src="http://www.phpnrio.com.br/img/mailling_cabecalho.png" />
-		<p>Olá!</p>
+		<table width="600"><tr><td width="600">
+			<p>Olá!</p>
 
-		<p>Recebemos um pagamento referente a <?=$plural? 'inscrições' : 'uma inscrição'?> para o PHP'n Rio, no nome de "<?=$transaction->name?>".<br />
-		Agora, para finalizar o processo, precisamos do <?=$plural? 'nome e RG das pessoas inscritas' : 'seu nome e RG'?>, para o credenciamento no dia do evento.
-		Também pedimos que você preencha, informalmente, quais são as palestras que você pretende assistir. Esses dados são necessários para que possamos separar as palestras nas salas disponíveis de acordo com o público esperado. Vale lembrar que a circulação pelas áreas do evento é totalmente livre, e essa informação é puramente estatística.</p>
+			<p>Recebemos um pagamento referente a <?=$plural? 'inscrições' : 'uma inscrição'?> para o PHP'n Rio, no nome de "<?=$transaction->name?>".<br />
+			Agora, para finalizar o processo, precisamos do <?=$plural? 'nome e RG das pessoas inscritas' : 'seu nome e RG'?>, para o credenciamento no dia do evento.
+			Também pedimos que você preencha, informalmente, quais são as palestras que você pretende assistir. Esses dados são necessários para que possamos separar as palestras nas salas disponíveis de acordo com o público esperado. Vale lembrar que a circulação pelas áreas do evento é totalmente livre, e essa informação é puramente estatística.</p>
 
-		<p>Para finalizar o processo, é necessário que você entre em <a href="http://www.phpnrio.com.br/grade" target="_blank">http://www.phpnrio.com.br/grade</a> e informe, no box da direita, o código da transação do PagSeguro.<br />
-		O seu código é <b><?=$transaction->code?></b></p>
+			<p>Para finalizar o processo, é necessário que você entre em <a href="http://www.phpnrio.com.br/grade" target="_blank">http://www.phpnrio.com.br/grade</a> e informe, no box da direita, o código da transação do PagSeguro.<br />
+			O seu código é <b><?=$transaction->code?></b></p>
 
-		<p>Não se esqueça de preencher os dados de inscrição, pois eles serão necessários para o credenciamento dos participantes. Também será necessário que todos os inscritos levem a identidade.</p>
+			<p>Não se esqueça de preencher os dados de inscrição, pois eles serão necessários para o credenciamento dos participantes. Também será necessário que todos os inscritos levem a identidade.</p>
 
-		<p>Em tempo: já viu nossa <a href="http://www.facebook.com/pages/PHPRio" target="_blank">página no Facebook</a>? Não esqueça também de marcar que <a href="http://www.facebook.com/event.php?eid=160928950657248" target="_blank">vai ao evento</a>!</p>
+			<p>Em tempo: já viu nossa <a href="http://www.facebook.com/pages/PHPRio" target="_blank">página no Facebook</a>? Não esqueça também de marcar que <a href="http://www.facebook.com/event.php?eid=160928950657248" target="_blank">vai ao evento</a>!</p>
 
 
-		<p><br />Agradecemos desde já, e um excelente evento pra você<?=$plural?'s':''?>!</p>
+			<p><br />Agradecemos desde já, e um excelente evento pra você<?=$plural?'s':''?>!</p>
 
-		<p>Equipe PHP'n Rio.</p>
+			<p>Equipe PHP'n Rio.</p>
+		</td></tr></table>
 		<img src="http://www.phpnrio.com.br/img/mailling_rodape.png" />
 	</body>
 </html>
