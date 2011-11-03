75f78f65f034d2838d15a598970064dc36ab68b2
igoru <igorsantos07@gmail.com>
2011-11-03 03:25:17 -0200

    fixing disclaimer about xml form

--- protected/modules/admin/views/transaction/_form.php
+++ protected/modules/admin/views/transaction/_form.php
@@ -6,6 +6,6 @@
 		echo CHtml::fileField('file');
 		echo CHtml::submitButton('Atualizar!');
 	?>
-	<p class="note">Subir um XML com novos registros enviará um email para cada um dos novos inscritos, informando sobre a página de finalização das inscrições.</p>
+	<p class="note">Subir um XML com novos registros enviará um email para cada um dos novos inscritos e os que tiveram seu pagamento confirmado, informando sobre a página de finalização das inscrições.</p>
 	<? echo CHtml::endForm(); ?>
 </div>
