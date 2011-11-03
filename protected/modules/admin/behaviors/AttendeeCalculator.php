3ffa0af56427ee84c185b25dc86255f55a590932
igoru <igorsantos07@gmail.com>
2011-11-03 03:23:40 -0200

    fixing count of unconfirmed attendees. closes #72

--- protected/modules/admin/behaviors/AttendeeCalculator.php
+++ protected/modules/admin/behaviors/AttendeeCalculator.php
@@ -7,7 +7,7 @@ class AttendeeCalculator extends CBehavior {
 			'unpaid_transactions'	=> Transaction::model()->count('status = ?',array(Transaction::STATUS_WAITING)),
 			'total_transactions'	=> Transaction::model()->count(),
 			'confirmed_attendees'	=> Attendee::model()->count(),
-			'unconfirmed_attendees'	=> Yii::app()->db->createCommand('SELECT COUNT(*) FROM transaction WHERE id NOT IN (SELECT transaction_id FROM attendee)')->queryScalar(),
+			'unconfirmed_attendees'	=> Yii::app()->db->createCommand('SELECT SUM(total_attendees) FROM transaction WHERE id NOT IN (SELECT transaction_id FROM attendee)')->queryScalar(),
 			'total_attendees'		=> Yii::app()->db->createCommand()->select('SUM(total_attendees)')->from('transaction')->queryScalar(),
 		);
 	}
