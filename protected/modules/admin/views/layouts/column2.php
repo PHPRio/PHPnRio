00f105964bb0a1a12032c677c58b0da99f2c12df
igoru <igorsantos07@gmail.com>
2011-11-03 00:27:36 -0200

    fixing links

--- protected/modules/admin/views/layouts/column2.php
+++ protected/modules/admin/views/layouts/column2.php
@@ -21,11 +21,11 @@
 					<td><?=$this->total_attendees['total_attendees']?></td>
 				</tr>
 				<tr>
-					<th><?=CHtml::link('Inscritos', array('attendees/index'))?></th>
+					<th><?=CHtml::link('Inscritos', array('attendee/index'))?></th>
 					<td><?=$this->total_attendees['confirmed_attendees']?></td>
 				</tr>
 				<tr>
-					<th><?=CHtml::link('Não-inscritos', array('trasactions/unconfirmedAttendees'))?></th>
+					<th><?=CHtml::link('Não-inscritos', array('transaction/unconfirmedAttendees'))?></th>
 					<td><?=$this->total_attendees['unconfirmed_attendees']?></td>
 				</tr>
 				<tr><td colspan="2"><hr /></td></tr>
