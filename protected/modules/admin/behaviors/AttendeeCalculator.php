cbd5051fffb81d914ddd135d5bfadf19aef318ba
igoru <igorsantos07@gmail.com>
2011-11-04 00:35:12 -0200

    fixing parametrization of sql queries

--- protected/modules/admin/behaviors/AttendeeCalculator.php
+++ protected/modules/admin/behaviors/AttendeeCalculator.php
@@ -3,8 +3,8 @@ class AttendeeCalculator extends CBehavior {
 
 	public function beforeAction() {
 		$this->owner->total_attendees = array(
-			'paid_transactions'		=> Transaction::model()->count('status = ?',array(Transaction::STATUS_APPROVED)),
-			'unpaid_transactions'	=> Transaction::model()->count('status = ?',array(Transaction::STATUS_WAITING)),
+			'paid_transactions'		=> Transaction::model()->count('status = :status',array(':status' => Transaction::STATUS_APPROVED)),
+			'unpaid_transactions'	=> Transaction::model()->count('status = :status',array(':status' => Transaction::STATUS_WAITING)),
 			'total_transactions'	=> Transaction::model()->count(),
 			'confirmed_attendees'	=> Attendee::model()->count(),
 			'unconfirmed_attendees'	=> Yii::app()->db->createCommand('SELECT SUM(total_attendees) FROM transaction WHERE id NOT IN (SELECT transaction_id FROM attendee)')->queryScalar(),
