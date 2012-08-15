1e89260bf3aaaa088f1d465fa30195af60329491
igoru <igorsantos07@gmail.com>
2012-08-15 10:29:09 -0300

    ! fixing yiic internal variables

--- protected/yiic.php
+++ protected/yiic.php
@@ -3,7 +3,7 @@ $env = getenv('ENVIRONMENT');
 define('ENV', $env? $env : 'prod');
 define('PRODUCTION', ENV == 'prod');
 
-$config = dirname(__FILE__).'/../protected/config/main.php';
-$yii = dirname(__FILE__).'/../yii/framework/'.((ENV == 'dev')? 'yii' : 'yiilite').'.php';
+$config = dirname(__FILE__).'/../protected/config/console.php';
+$yiic = dirname(__FILE__).'/../yii/framework/yiic.php';
 
 require_once($yiic);
