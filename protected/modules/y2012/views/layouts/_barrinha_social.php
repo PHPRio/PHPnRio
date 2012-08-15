a326a9c129077fe0761d9afa682037cf1ee94b41
igoru <igorsantos07@gmail.com>
2012-08-15 11:13:02 -0300

    ! fixing social bar style

--- protected/modules/y2012/views/layouts/_barrinha_social.php
+++ protected/modules/y2012/views/layouts/_barrinha_social.php
@@ -1,13 +1,14 @@
-<div class="redessociais" style="padding-left: 30px">
-	<span style="float:left; width: 33%; height: 100%">
+<div class="redessociais">
+    <span style="float:left; width: 33%; height: 100%">
 		<fb:like href="http://www.facebook.com/pages/PHPRio/160383237381004" send="false" layout="button_count" width="110" show_faces="false" font=""></fb:like>
 	</span>
-	&nbsp;
+
 	<span style="float:left; width: 33%; height: 100%">
-		<a href="http://twitter.com/share" class="twitter-share-button" data-count="horizontal" data-via="phprio" data-text="PHP'n Rio 2011, dia 5 de Novembro - O evento de desenvolvimento web carioca" data-lang="pt"></a>
+		<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://phpnrio.com.br" data-text="Está aberta a pré-inscrição para o PHP'n Rio 2012, vai ficar de fora?" data-lang="pt" data-count="none" data-hashtags="PHPnRio">Tweetar</a>
+		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 	</span>
-	&nbsp;
-	<span style="float:left; width: 33%; height: 100%; margin-top: -19px">
+
+	<span style="float:left; width: 33%; height: 100%">
 		<g:plusone size="medium" href="http://www.phpnrio.com.br"></g:plusone>
 	</span>
 </div>
