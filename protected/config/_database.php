4e81f981b3cdf3b1bee4575633b49feb768b9306
Igor Santos <igorsantos07@gmail.com>
2012-08-15 17:30:41 -0300

    ! trying to fix port problems with mysql connection

--- protected/config/_database.php
+++ protected/config/_database.php
@@ -3,7 +3,7 @@ return array(
 	'2011' => array(
 		'class' => 'CDbConnection',
 		'connectionString' => PRODUCTION?
-			"mysql:host={$_SERVER['DB1_HOST']};mysql:port={$_SERVER['DB1_PORT']};dbname={$_SERVER['DB1_NAME']}" :
+			"mysql:host={$_SERVER['DB1_HOST']};port={$_SERVER['DB1_PORT']};dbname={$_SERVER['DB1_NAME']}" :
 			'mysql:host=localhost;dbname=phpnrio2011',
 		'username' => PRODUCTION? $_SERVER['DB1_USER'] : 'phpnrio2011',
 		'password' => PRODUCTION? $_SERVER['DB1_PASS'] : 'phpnrio',
@@ -13,7 +13,7 @@ return array(
 	'2012' => array(
 		'class' => 'CDbConnection',
 		'connectionString' => PRODUCTION?
-			"mysql:host={$_SERVER['DB2_HOST']};mysql:port={$_SERVER['DB2_PORT']};dbname={$_SERVER['DB2_NAME']}" :
+			"mysql:host={$_SERVER['DB2_HOST']};port={$_SERVER['DB2_PORT']};dbname={$_SERVER['DB2_NAME']}" :
 			'mysql:host=localhost;dbname=phpnrio2012',
 		'username' => PRODUCTION? $_SERVER['DB2_USER'] : 'phpnrio2011',
 		'password' => PRODUCTION? $_SERVER['DB2_PASS'] : 'phpnrio',
