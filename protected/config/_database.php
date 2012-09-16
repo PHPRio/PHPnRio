5106f3349d60890ddfd4de7f39de414e60f7a7d9
Igor Santos <igorsantos07@gmail.com>
2012-09-16 15:38:44 -0300

    * setting the database user to something more generic, without the year on it

--- protected/config/_database.php
+++ protected/config/_database.php
@@ -5,7 +5,7 @@ return array(
 		'connectionString' => PRODUCTION?
 			"mysql:host={$_SERVER['DB1_HOST']};port={$_SERVER['DB1_PORT']};dbname={$_SERVER['DB1_NAME']}" :
 			'mysql:host=localhost;dbname=phpnrio2011',
-		'username' => PRODUCTION? $_SERVER['DB1_USER'] : 'phpnrio2011',
+		'username' => PRODUCTION? $_SERVER['DB1_USER'] : 'phpnrio',
 		'password' => PRODUCTION? $_SERVER['DB1_PASS'] : 'phpnrio',
 		'emulatePrepare' => true,
 		'charset' => 'utf8',
@@ -15,7 +15,7 @@ return array(
 		'connectionString' => PRODUCTION?
 			"mysql:host={$_SERVER['DB2_HOST']};port={$_SERVER['DB2_PORT']};dbname={$_SERVER['DB2_NAME']}" :
 			'mysql:host=localhost;dbname=phpnrio2012',
-		'username' => PRODUCTION? $_SERVER['DB2_USER'] : 'phpnrio2011',
+		'username' => PRODUCTION? $_SERVER['DB2_USER'] : 'phpnrio',
 		'password' => PRODUCTION? $_SERVER['DB2_PASS'] : 'phpnrio',
 		'emulatePrepare' => true,
 		'charset' => 'utf8',
